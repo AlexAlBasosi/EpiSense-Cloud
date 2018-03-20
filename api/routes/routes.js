98f5fa1814c93a47615f492fc73ff844bb8acaca
Alexander Al Basosi <tazlex15@gmail.com>
2018-03-20 17:09:14 +0400

    added route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 
     var controller = require('../controllers/controller');
 
-    app.route('/records')
+    app.route('/patients')
       .get(controller.get_all_records)
       .post(controller.add_a_record)
 
