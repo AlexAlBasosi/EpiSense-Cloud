170a5bb37fa1c98ad5522b49ec2b152a0912f114
Alexander Al Basosi <tazlex15@gmail.com>
2018-04-19 01:50:18 +0400

    integrated signup page with backend

--- public/js/index.js
+++ public/js/index.js
@@ -42,7 +42,38 @@ $('.tab a').on('click', function (e) {
   
 });
 
- const loginForm = document.forms['login-form'];
+
+const signUpForm = document.forms['signup-form'];
+
+signUpForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  const fname = signUpForm.querySelector('input[id=firstName]').value;
+  const lname = signUpForm.querySelector('input[id=lastName]').value;
+  const email = signUpForm.querySelector('input[id=email]').value;
+  const password = signUpForm.querySelector('input[id=password]').value;
+  const speciality = signUpForm.querySelector('input[id=speciality]').value;
+
+  var signUpUrl = "/doctors?doctor_Id=5&email=" + email + "&first_name=" + fname + "&last_name=" + lname + "&specialization=" + speciality + "&doctor_password=" + password;
+
+  console.log(signUpUrl);
+
+  $.ajax({
+    type: "POST",
+    url: signUpUrl,
+    dataType: "text",
+    success: function (msg) {
+      if (msg) {
+        alert("Signup Successful!");
+        location.reload(true);
+      } else {
+        alert("Signup failed.");
+        }
+      },
+  });
+
+});
+
+const loginForm = document.forms['login-form'];
 
 loginForm.addEventListener('submit', function(event){
     event.preventDefault();
@@ -67,13 +98,3 @@ loginForm.addEventListener('submit', function(event){
         },
     });
 });
-
-// function login(){
-//   var user = $("user").val();
-//   console.log(user);
-// }
-
-// function submit(){
-//   console.log("potato");
-//   alert("poato");
-// }
